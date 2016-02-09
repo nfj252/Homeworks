4d74042f9e38d72164898c4529487885d7a66f11
Nicholas Fu <njf252@nyu.edu>
2016-02-08 22:12:30 -0500

    Nicholas Fu HW 1

---
+++ HW_1/NYUCodebase/main.cpp
@@ -0,0 +1,119 @@
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include "ShaderProgram.h"
+#include "Matrix.h"
+
+#ifdef _WINDOWS
+#define RESOURCE_FOLDER ""
+#else
+#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
+#endif
+
+SDL_Window* displayWindow;
+
+GLuint LoadTexture(const char *image_path) 
+{
+	SDL_Surface *surface = IMG_Load(image_path);
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA,
+		GL_UNSIGNED_BYTE, surface->pixels);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	SDL_FreeSurface(surface);
+	return textureID;
+}
+
+int main(int argc, char *argv[])
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	displayWindow = SDL_CreateWindow("Rocket Going Through Orbit", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+	SDL_GL_MakeCurrent(displayWindow, context);
+#ifdef _WINDOWS
+	glewInit();
+#endif
+
+	SDL_Event event;
+	ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
+	Matrix projectionMatrix;
+	Matrix modelMatrix;
+	Matrix modelMatrixMobile;
+	Matrix viewMatrix;
+	float vertices1[] = {-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5};
+	float vertices2[] = {-2.5, -0.5, -1.5, -0.5, -1.5, 0.5, -2.5, -0.5, -1.5, 0.5, -2.5, 0.5};
+	float vertices3[] = {1.5, -0.5, 2.5, -0.5, 2.5, 0.5, 1.5, -0.5, 2.5, 0.5, 1.5, 0.5};
+	float texCoords[] = {0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0};
+
+	glViewport(0, 0, 640, 360);   //axis starts from lower left (x,y,w,h)
+	glUseProgram(program.programID);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	projectionMatrix.setOrthoProjection(-3.55f, 3.55f, -2.0f, 2.0f, -1.0f, 1.0f); //(l,r,b,t,n,f) n/f doesnt do anything in ortho
+
+	GLuint texture1 = LoadTexture("alienBlue_square.png");
+	GLuint texture2 = LoadTexture("alienPink_square.png");
+	GLuint texture3 = LoadTexture("alienYellow_square.png");
+
+	modelMatrix.identity();
+	
+	float lastFrameTicks = 0.0f;
+	float ticks = 0.0f;
+	float elapsed = 0.0f;
+
+	bool done = false;
+	while (!done) 
+	{
+		ticks = (float)SDL_GetTicks() / 1000.0f;
+		elapsed = ticks - lastFrameTicks;
+		lastFrameTicks = ticks;
+
+		glClearColor(0.4f, 0.2f, 0.4f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		while (SDL_PollEvent(&event)) 
+		{
+			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) 
+				done = true;
+		}
+		
+		program.setModelMatrix(modelMatrix);
+		program.setProjectionMatrix(projectionMatrix);
+		program.setViewMatrix(viewMatrix);
+
+		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices1);
+		glEnableVertexAttribArray(program.positionAttribute);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glBindTexture(GL_TEXTURE_2D, texture2);
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices2);
+		glEnableVertexAttribArray(program.positionAttribute);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+	
+		program.setModelMatrix(modelMatrixMobile);
+		glBindTexture(GL_TEXTURE_2D, texture3);
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices3);
+		glEnableVertexAttribArray(program.positionAttribute);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		modelMatrixMobile.Translate(0.0f, elapsed, 0.0f);
+
+		glDisableVertexAttribArray(program.positionAttribute);
+		glDisableVertexAttribArray(program.texCoordAttribute);
+
+		SDL_GL_SwapWindow(displayWindow);
+	}
+
+	SDL_Quit();
+	return 0;
+}
